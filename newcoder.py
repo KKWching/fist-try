b652a5b4a321b2fdb9495ad2d3fa321dc30dc429
wangchen49 <wangchen49@MacBook-Pro-56.local>
2023-01-17 19:09:23 +0800

    第一次提交

---
+++ newcoder.py
@@ -0,0 +1,32 @@
+# operators_dict = {'<': 'less than', '==': 'equal'}
+# print('Here is the original dict:')
+# sorted(operators_dict)
+# for x, y in operators_dict.items():
+#     print(f'Operator {x} means {y}.')
+# operators_dict['>'] = 'greater than'
+# print()
+# # 输出一个换行！！！print('\n')输出两个换行
+# print('The dict was changed to:')
+# sorted(operators_dict)
+# for x, y in operators_dict.items():
+#     print(f'Operator {x} means {y}.')
+
+
+# cities_dict = {'Beijing': {'Capital': 'China'}, 'Moscow': {'Capital': 'Russia'}, 'Paris': {'Capital': 'France'}}
+# sorted(cities_dict)
+# for x,y in cities_dict.items():
+#     # print(x,y['Capital'])
+#     # print(f'{x} is the capital of {y['Capital']}!')
+#     print(f"{x} is the capital of {y['Capital']}!")
+#     # 格式化输出时，一定要注意单双引号的层次！！！
+#     # print(x + ' is the capital of ' + y['Capital'] + '!')
+
+
+result_dic = {'Allen': ['red', 'blue', 'yellow'], 'Tom': ['green', 'white', 'blue'], 'Andy': ['black', 'pink']}
+# sorted(result_dic)
+# sorted(result_dic.items(), key=lambda e: e[0])
+print(sorted(result_dic))
+# for name, color in sorted(result_dic.items()):
+#     print(f"{name}'s favorite colors are:")
+#     for i in color:
+#         print(i)
